dd4ebccee37addfe91e108478d4c98792b991787
lazysoul <i@lazysoul.com>
2020-08-05 17:56:55 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JetpackNavigation
-Android Jetpack Navigation
+
+Android Jetpack Navigation 구현하기에서 사용한 코드 입니다. 
+
+https://velog.io/@lazysoul/jetpack-navigation
+
---
+++ app/src/main/java/io/whatap/navigation/ThirdFragment.kt
@@ -0,0 +1,23 @@
+package io.whatap.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class ThirdFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_third, container, false)
+
+        view.findViewById<Button>(R.id.button3).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_thirdFragment_to_firstFragment)
+        }
+        return view
+    }
+}
